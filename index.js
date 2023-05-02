9f08f028b3005eeecd3b8f90e2c89357978d294e
Shafiulislam <ishafiul900@gmail.com>
2023-05-02 19:26:51 +0600

    shofol cook all jason data creat done

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const country = require('./data/chef-country.json'); 
+const chef = require('./data/chef.json');
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("shofol is running")
 });
+
+app.get('/chef', (req, res) => {
+    res.send(chef);
+})
+
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const selectChef = 
+    console.log(id);
+})
 
 app.get('/chef-country', (req, res) => {
     res.send(country);
