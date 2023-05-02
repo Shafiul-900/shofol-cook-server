ff963ee6f884f7c3bcb77f8a52c12c735657a006
Shafiulislam <ishafiul900@gmail.com>
2023-05-02 16:14:08 +0600

    create local host 5000 and country data loding complit

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
+const express = require('express');
 const app = express()
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+const country = require('./data/chef-country.json'); 
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("shofol is running")
 });
+
+app.get('/chef-country', (req, res) => {
+    res.send(country);
+})
 
 app.listen(port, () => {
     console.log(`shofol Api is running on port: ${port}`)
